907bc49a6a9077a73f03556885a37712b62e50ab
Carlos Soria <csoria@cultome.io>
2021-03-17 23:00:35 -0600

    Update cookie value

--- web.rb
+++ web.rb
@@ -8,19 +8,25 @@ class InkStat < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get '/extract' do
-    cookie = params.fetch('cookie', 'ebb3e0c264fd267aeaa0c6585089216e19e7f01f')
+    cookie = params.fetch('cookie', 'bf8f7fb17733e383f7526382acb3dcc6bf4b7053')
 
     data = extractor(cookie).fetch_battles
     battles = parser.parse data
     inserted = storage.save battles
 
     { inserted: inserted }.to_json
+  rescue Exception => error
+    halt 500, { error: error.message }.to_json
   end
 
   get '/init' do
     storage.create_schema!
 
     { message: 'schema created' }.to_json
+  end
+
+  get '/version' do
+    { version: '1.0.0' }.to_json
   end
 
   get '/drop' do
